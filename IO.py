97f8cff282a0abde37539cf04ee459c02d8ef159
Esmidth <esmidth@163.com>
2016-02-05 18:09:36 +0800

    Reformat Windows Version on Mac

--- IO.py
+++ IO.py
@@ -6,29 +6,29 @@ import StringHandler as sh
 
 
 def write(fileName, id):
-    df = ts.get_hist_data(id)  # , start='2000-01-01')
-    if type(df) != type(None):
-        df = df.sort_index(ascending=True)
-        df.to_excel(fileName, sheet_name='Sheet1', index=True, index_label='date', merge_cells=False)
-        print("%s Done" % id)
-        # return 1
-    else:
-        print("%s Fail" % id)
-        #return 0
+	df = ts.get_hist_data(id)  # , start='2000-01-01')
+	if type(df) != type(None):
+		df = df.sort_index(ascending=True)
+		df.to_excel(fileName, sheet_name='Sheet1', index=True, index_label='date', merge_cells=False)
+		print("%s Done" % id)
+	# return 1
+	else:
+		print("%s Fail" % id)
+	# return 0
 
 
 def load(fileName):
-    return pd.read_excel(fileName, 'Sheet1', index_col='date')
+	return pd.read_excel(fileName, 'Sheet1', index_col='date')
 
 
 def downloadAll(date):
-    path = 'DataBase_' + date.__str__()
-    for x in sh.lists:
-        write(path + '\\' + x + '.xlsx', x)
-    print("Download finished")
+	path = 'DataBase_' + date.__str__()
+	for x in sh.lists:
+		write(path + '\\' + x + '.xlsx', x)
+	print("Download finished")
 
 
 if __name__ == '__main__':
-    print("timer started:%s" % time.clock())
-    downloadAll(20151128)
-    print("timer ended:%s" % time.clock())
+	print("timer started:%s" % time.clock())
+	downloadAll(20151128)
+	print("timer ended:%s" % time.clock())
